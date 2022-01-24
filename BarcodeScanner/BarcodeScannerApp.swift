82c8b4ab8338eab971b9a9b6a944d174db0be47e
Ivan Trj <ivantrajanovski@gmail.com>
2022-01-24 21:04:39 +0100

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Ivan Trajanovski on 2022. 01. 24..
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
